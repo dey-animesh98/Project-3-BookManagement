f9c77df62791a7a4d465fe96cf0a61e1a6aa2348
Animesh Dey <animesh.dey986@gmail.com>
2022-05-17 08:40:32 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-const express= require('express')
+const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const route = express.Router()
 const app = express()
 const port = 3000
+const route = require('./Routers/routes');
+
+
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/group32Database",  {
-    useNewUrlParser: true})
+mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/group32Database", {
+    useNewUrlParser: true
+})
 
-.then(()=>console.log("MongoDB is Connected."))
-.catch((err)=>console.log(err.message))
+    .then(() => console.log("MongoDB is Connected."))
+    .catch((err) => console.log(err.message))
 
-app.listen(process.env.PORT || port, function(){
+app.use('/', route);
+
+
+app.listen(process.env.PORT || port, function () {
     console.log("Express app is running on ", process.env.PORT || port)
 })
